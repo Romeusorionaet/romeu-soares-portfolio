07269bc8d4e8b21c5dea16572afcc4069d8cf944
romeusorionaet <romeusoares14569@gmail.com>
2024-07-22 14:09:37 -0300

    chore: changed logic to play videos / added new videos

--- src/components/section-videos-on-topics/index.tsx
+++ src/components/section-videos-on-topics/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useRef } from 'react'
+import { useRef, useEffect } from 'react'
 import Image from 'next/image'
 import bg2 from '../../assets/img/backgrounds-img/bg-2.png'
 import { MovingStars } from '../moving-card/moving-stars'
@@ -8,54 +8,63 @@ import { MovingStars } from '../moving-card/moving-stars'
 export function SectionVideosOnTopics() {
   const videoRefs = useRef<(HTMLVideoElement | null)[]>([])
 
-  const handleMouseOver = () => {
-    videoRefs.current.forEach((video) => {
-      if (video) {
-        video.play().catch((error) => {
-          console.error('Failed to play video:', error)
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          const video = entry.target as HTMLVideoElement
+          if (entry.isIntersecting) {
+            video.play().catch((error) => {
+              console.error('Failed to play video:', error)
+            })
+          } else {
+            video.pause()
+            video.currentTime = 0
+          }
         })
-      }
-    })
-  }
+      },
+      {
+        threshold: 1,
+      },
+    )
 
-  const handleMouseOut = () => {
-    videoRefs.current.forEach((video) => {
+    const currentVideoRefs = videoRefs.current
+
+    currentVideoRefs.forEach((video) => {
       if (video) {
-        video.pause()
-        video.currentTime = 0
+        observer.observe(video)
       }
     })
-  }
+
+    return () => {
+      currentVideoRefs.forEach((video) => {
+        if (video) {
+          observer.unobserve(video)
+        }
+      })
+    }
+  }, [])
 
   const videos = [
     {
       id: 1234,
       src: '/video/boy-end-book.mp4',
-      title: 'livros',
+      title: 'Livros',
     },
     {
       id: 2345,
-      src: '/video/boy-end-book.mp4',
-      title: 'livros',
+      src: '/video/example-movie.mp4',
+      title: 'Filmes',
     },
     {
-      id: 3234,
-      src: '/video/boy-end-book.mp4',
-      title: 'livros',
-    },
-    {
-      id: 4457,
-      src: '/video/boy-end-book.mp4',
-      title: 'livros',
+      id: 7992,
+      src: '/video/astronomia-astrologia.mp4',
+      title: 'Astronomia / Astrologia',
     },
   ]
 
   return (
-    <section
-      onMouseOver={handleMouseOver}
-      onMouseOut={handleMouseOut}
-      className="section_limiter relative mt-8 overflow-hidden rounded-lg p-4 text-foreground"
-    >
+    <section className="section_limiter relative mt-8 overflow-hidden rounded-lg p-4 text-foreground">
       <Image
         width={36}
         height={36}
@@ -67,29 +76,34 @@ export function SectionVideosOnTopics() {
 
       <h2 className="mb-4 text-2xl font-bold">Temas variados</h2>
 
-      <p>Preparei muito mais do que apenas conversar sobre programação!</p>
+      <p>Preparei alguns temas que me interessam.</p>
 
-      {Array.from({ length: 30 }).map((_, index) => (
+      {Array.from({ length: 20 }).map((_, index) => (
         <MovingStars key={index} />
       ))}
 
-      <div className="flex w-full flex-wrap justify-center gap-4 py-8">
+      <div className="flex w-full flex-wrap justify-center gap-2 py-8 md:gap-4">
         {videos.map((item, index) => {
           return (
             <div
               key={item.id}
               className="group relative h-[5.5rem] w-[10rem] overflow-hidden rounded-md md:h-[8rem] md:w-[14rem] lg:h-[16rem] lg:w-[28rem]"
             >
-              <h3 className="absolute bottom-0 left-2 z-10 font-slackey text-sm font-bold lg:text-base">
+              <h3 className="absolute bottom-0 left-2 z-10 font-slackey text-xs font-bold lg:text-base">
                 {item.title}
               </h3>
               <video
                 ref={(el) => {
                   videoRefs.current[index] = el
                 }}
-                className="h-full w-full duration-700 group-hover:scale-125"
+                className="h-full w-full border-4 border-transparent object-cover duration-700 focus:border-white group-hover:scale-125"
                 muted
                 loop
+                onMouseEnter={(e) => {
+                  e.currentTarget.play().catch((error) => {
+                    console.error('Failed to play video:', error)
+                  })
+                }}
               >
                 <source src={item.src} type="video/mp4" />
                 Your browser does not support the video tag.
