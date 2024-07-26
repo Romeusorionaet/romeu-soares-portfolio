702b4827a08e8fe462a496e4143608da95b5316e
romeusorionaet <romeusoares14569@gmail.com>
2024-07-26 20:29:44 -0300

    feat: applied logic for show previews of projects, mobile or desktop

--- src/app/(portfolio)/my-projects/components/card-project.tsx/index.tsx
+++ src/app/(portfolio)/my-projects/components/card-project.tsx/index.tsx
@@ -36,35 +36,15 @@ export function CardProject({
   }, [previewDesktop, previewMobile])
 
   return (
-<<<<<<< HEAD
-    <div className="flex h-[28rem] w-52 flex-col items-center rounded-md p-2 lg:h-[16rem] lg:w-[24rem]">
-=======
     <div className="flex h-72 w-96 flex-col items-center rounded-md p-2">
->>>>>>> ead0990 (fixed value css from layouts)
       <h2>{title}</h2>
 
       <a
         href={pageURL}
         target="_blank"
-<<<<<<< HEAD
-        className="h-full w-full duration-700"
-=======
         className="h-44 w-full duration-700"
->>>>>>> ead0990 (fixed value css from layouts)
         rel="noreferrer"
       >
-<<<<<<< HEAD
-        {preview && (
-          <Image
-            width={36}
-            height={36}
-            sizes="100vw"
-            className="mx-auto h-full w-full rounded-md border-4 border-transparent object-contain"
-            src={preview}
-            alt={`Preview of ${title}`}
-          />
-        )}
-=======
         <Image
           width={36}
           height={36}
@@ -73,7 +53,6 @@ export function CardProject({
           src={previewDesktop}
           alt="Loading animation"
         />
->>>>>>> 7eb89d7 (refactor: created function helper and constants for topics)
       </a>
     </div>
   )
