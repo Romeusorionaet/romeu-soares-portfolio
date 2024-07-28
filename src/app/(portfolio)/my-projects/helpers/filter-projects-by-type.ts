15f4a32f3473920344143c3c6062696acfee2929
romeusorionaet <romeusoares14569@gmail.com>
2024-07-28 14:20:07 -0300

    fix: fixed path for interface 'ProjectsProps' and changed favicon

--- src/app/(portfolio)/my-projects/helpers/filter-projects-by-type.ts
+++ src/app/(portfolio)/my-projects/helpers/filter-projects-by-type.ts
@@ -1,4 +1,4 @@
-import { ProjectsProps } from '@/actions/firebase/fetch-projects'
+import { ProjectsProps } from '@/actions/firebase'
 
 export function filterProjectsByType(projects: ProjectsProps[], type: string) {
   return projects.filter((item) => item.type === type)
